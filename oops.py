69cf2bf4c79b4790a87a7813646ea74ae07746b7
Shubhranshu-Malhotra <shubhranshumalhotra.sm@gmail.com>
2021-03-13 00:15:46 +0530

    Oops - Lecture 117

--- oops.py
+++ oops.py
@@ -1,16 +1,21 @@
 # Udemy: Python Developer in 2021: Zero to Mastery
 
-# Lecture 116
+# Lecture 116, 117
 
 
 class PlayerCharacter:
+    membership = True # This is a class attribute
     def __init__(self, name, age=10):
-        self.name = name
-        self.age = age
+        if (self.membership == True): # 2 ways to access class attribute - way 1 - "self.membership" 
+            self.name = name # This is an object attribute, cant access by class name "PlayerCharacter.name"
+            self.age = age # object attribute
+        elif (PlayerCharacter.membership == False): # Way 2 - "PlayerCharacter.membership"  (self not required)
+            print("Please get a membership")
     def run(self):
         print('run')
         return 'done'
-
+    def shout(self):
+        print(f"your name is {self.name}, You are a member: {PlayerCharacter.membership}")
 player1 = PlayerCharacter('Anna')
 print("Player 1 Details: ",player1.name, player1.age)
 player2 = PlayerCharacter('Rohan', 22)
@@ -18,6 +23,12 @@ player2 = PlayerCharacter('Rohan', 22)
 print(player2.name,  player2.run(),  player2.age)
 player2.attack = 150
 print(player2.attack)
-print(player1.attack) # throws error since attack attribute defined only for player 2
+player1.shout()
+# print(player1.attack) # throws error since attack attribute defined only for player 2
+
+
+# Lecture 117
+
+
 
 
