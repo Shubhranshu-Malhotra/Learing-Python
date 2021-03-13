ee3f9c59ac87dd51ac7abc5127b2caad69f440e2
Shubhranshu-Malhotra <shubhranshumalhotra.sm@gmail.com>
2021-03-13 19:14:34 +0530

    Inheritance 2-Lecture 127

--- Oops_Inheritance.py
+++ Oops_Inheritance.py
@@ -1,4 +1,4 @@
-class User:
+class User: # Parent Class
     
     def __init__(self, name, power):
         self.name = name
@@ -7,17 +7,11 @@ class User:
     def sign_in(self):
         print('Logged In.')
 
-class Wizard (User):
-    # def __init__(self, name, power):
-    #     self.name = name
-    #     self.power = power
+class Wizard (User): # Sub Class / Derived CLass
     def abilities(self):
         print('Wizard abilities: Ice javelin, Fire bomb')
 
 class Archer (User):
-    # def __init__(self, name, num_arrows):
-    #     self.name = name
-    #     self.power = power
     def abilities(self):
         print('Archer abilities: Fire arrow, Earth arrow')
 
@@ -34,3 +28,13 @@ wizard1.abilities()
 print('Archer: ', archer1.name, archer1.power)
 archer1.sign_in()
 archer1.abilities()
+
+
+print('Checking instace of which classes')
+print('Check if wizard1 is instance of Wizard Class: ', isinstance(wizard1, Wizard))
+print('Check if wizard1 is instance of User Class: ', isinstance(wizard1, User))
+print('Check if wizard1 is instance of object Class: ', isinstance(wizard1, object))
+print('Check if wizard1 is instance of Archer Class: ', isinstance(wizard1, Archer))
+print('Check if Archer is instance of object Class: ', isinstance(Archer, object))
+
+
