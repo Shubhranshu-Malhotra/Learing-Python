0d0a82ccc413ccd71ceb89c3ca6fa38dcc0d25d0
Shubhranshu-Malhotra <shubhranshumalhotra.sm@gmail.com>
2021-03-17 19:40:34 +0530

    python input output basics

---
+++ python_I_O.py
@@ -0,0 +1,15 @@
+test = open('test.txt')
+print(test.read())
+print(test.read()) # shows nothing since cursor at the end of file
+print(test.read()) # shows nothing since cursor at the end of the file
+test.seek(0) # to return the cursor to the start of the file
+print(test.read()) # Now we are able to print again.
+test.seek(0) # to return the cursor to the start of the file
+print(test.read())
+test.seek(0)
+print(test.readline())
+print(test.readline())
+print(test.readline())
+test.seek(0)
+print(test.readlines())
+test.close()
