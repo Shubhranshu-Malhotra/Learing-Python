ca5da68c5b3dbce898c37cbd78b72c3988939cd7
Shubhranshu-Malhotra <shubhranshumalhotra.sm@gmail.com>
2021-03-18 14:22:52 +0530

    -

--- regular_expressions.py
+++ regular_expressions.py
@@ -10,9 +10,19 @@ print(a.group()) # Don't really understand its usage.
 
 pattern = re.compile('This')
 pattern2 = re.compile('Hi! This is me learning python! This is self learning.')
+
 b = pattern.search(string)
 print(b)
 print(pattern.findall(string))
 print(pattern.fullmatch(string)) # Matches only if the exact same string
 print(pattern2.fullmatch(string)) # Matches only if the exact same string
 print(pattern2.match(string)) # Matches the exact string in pattern to one passed and doen't care if the passed string is different afterwards
+
+pattern3 = re.compile(r'([a-zA-Z]).([a])')
+x = pattern3.search(string)
+print(x)
+print(x.group(1))
+print(x.group(2))
+
+pattern4 = re.compile(r'^Hi')
+print(pattern4.search(string))
